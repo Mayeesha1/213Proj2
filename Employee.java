cf3bc56aa3950cf00195ce43002c4b68dae0fdf0
craziibunny <mm2744@scarletmail.rutgers.edu>
2021-02-17 03:26:54 -0500

    i didnt do too much, im still trying to figure things out but here is the updated code :)

--- Employee.java
+++ Employee.java
@@ -1,7 +1,18 @@
 
+/**
+The employee class holds all the main attributes of an employee. 
+(add more after i understand more LOL)...
+@author mayeesha, rebecca
+*/
 public class Employee {
 	
-	public void calculatePayment() { } 
+	/**
+	This method calculates the payments for an employee, keeping 
+	in mind the total hours they worked. (fix if im wrong later...)
+	 */
+	public void calculatePayment() {
+		
+	} 
 	@Override
 	public boolean equals(Object obj) {
 		return true; //just for now to make the error go away
--- PayrollProcessing.java
+++ PayrollProcessing.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 import java.util.StringTokenizer;
 
 /**
-The payrollprocessing class is used to run all the commands in the company. Based on 
-the inputted commands, the payrollprocessing class uses the company methods as needed 
+The PayrollProcessing class is used to run all the commands in the company. Based on 
+the input commands, the PayrollProcessing class uses the company methods as needed 
 to add, remove, calculate payments, set working hours, and print earning statements 
-from the employee list. This class also handles exceptions through the user input.
+from the employee list. This class also handles exceptions from the user input.
 @author mayeesha, rebecca
 */
 public class PayrollProcessing {
@@ -24,14 +24,29 @@ public class PayrollProcessing {
 		Scanner scanner=new Scanner(System.in);
 		String line=scanner.nextLine();
 		company = new Company();
+		try {
 		while(!line.equals("Q")) {
-			StringTokenizer st=new StringTokenizer(line,",",false);
+			StringTokenizer st=new StringTokenizer(line," ",false);
 			String command=st.nextToken(); 
-			if(command.equals("AP")) {
+		
+			if(command.equals("AP")) { //part-time employee w hourly pay rate
+			String name = st.nextToken();
+			String depCode = st.nextToken();
+			String date = st.nextToken();
+			String moneyEarned = st.nextToken();
+			System.out.println("name of employee is: " + name); //these are to test for now
+			System.out.println("depCode of employee is: " + depCode);
+			System.out.println("date joined of employee is: " + date);
+			System.out.println("earnings of employee is: " + moneyEarned);
+			if((depCode.equals("CS")) || (depCode.equals("ECE")) || (depCode.equals("IT"))) {
+			    //call add method here and also check if employee already exists or not
+			} else {
+				System.out.println("invalid department code");
+			}
+			
+			} else if(command.equals("AF")) { //full-time employee w annual salary
 				
-			} else if(command.equals("AF")) {
-				
-			} else if(command.equals("AM")) {
+			} else if(command.equals("AM")) { //full-time employee w diff roles
 				
 			} else if(command.equals("R")) {
 				
@@ -39,18 +54,23 @@ public class PayrollProcessing {
 				
 			} else if(command.equals("S")) {
 				
-			} else if(command.equals("PA")) {
+			} else if(command.equals("PA")) { //earnings for all employees
 				
-			} else if(command.equals("PH")) {
+			} else if(command.equals("PH")) { //earnings by date hired
 				
-			} else if(command.equals("PD")) {
+			} else if(command.equals("PD")) { //earnings grouped by department
 				
 			} else {
 				System.out.println("Invalid command!");
 			}
 			line = scanner.nextLine();	
-		}
+		 }
 		System.out.println("Payroll Processing completed."); //quit
+
+		}
+		 catch (Exception e) {
+			   System.out.println("Invalid input.");
+			 }
 		
 		scanner.close();
 	} 
