ce7df7897d17c5f3bd0b67d7bc3c45b0f27ebd1c
craziibunny <mm2744@scarletmail.rutgers.edu>
2021-02-23 02:57:10 -0500

    semi final code hehe

--- RunProject2.java
+++ RunProject2.java
@@ -1,6 +1,11 @@
 
+/**
+The class is in charge of running everything all together by calling the method from the user I/O 
+class, payroll processing. This class holds the main method to run the code. 
+@author mayeesha, rebecca
+*/
 public class RunProject2 {
-
+	
 	public static void main(String[] args) {
 		new PayrollProcessing().run();
 	}
